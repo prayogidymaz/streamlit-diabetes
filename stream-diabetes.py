4be0ab1f659476adc3091a8d5bfc9d727cb93478
prayogidymaz <76582586+prayogidymaz@users.noreply.github.com>
2025-06-01 14:03:00 +0700

    stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -11,35 +11,51 @@ st.title("Data Mining Prediksi Diabetes")
 col1, col2 = st.columns(2)
 
 with col1 :
-    Pregnancies = st.text_input ("Input nilai pregnancies")
+    #Pregnancies = st.text_input ("Input nilai pregnancies")
+    Pregnancies = float(st.text_input("Input nilai pregnancies") or 0)
+
 
 with col2 :
-    Glucose = st.text_input ("Input nilai Glucose")
+    #Glucose = st.text_input ("Input nilai Glucose")
+    Glucose = float(st.text_input("Input nilai glucose") or 0)
+
 
 with col1 :
-    Bloodpressure = st.text_input ("Input nilai Bloodpressure")
+    #Bloodpressure = st.text_input ("Input nilai Bloodpressure")
+    BloodPressure = float(st.text_input("Input nilai blood plessure") or 0)
+
 
 with col2 :
-    SkinThickness = st.text_input ("Input nilai SkinThickness")
+    #SkinThickness = st.text_input ("Input nilai SkinThickness")
+    SkinThickness = float(st.text_input("Input nilai skin thickness") or 0)
+
 
 with col1 :
-    Insulin = st.text_input ("Input nilai Insulin")
+    #Insulin = st.text_input ("Input nilai Insulin")
+    Insulin = float(st.text_input("Input nilai insulin") or 0)
+
 
 with col2 :
-    BMI = st.text_input ("Input nilai BMI")
+    #BMI = st.text_input ("Input nilai BMI")
+    BMI = float(st.text_input("Input nilai BMI") or 0)
+
 
 with col1 :
-    DiabetesPedigreeFunction = st.text_input ("Input nilai Diabetes Pedigree Function")
+    #DiabetesPedigreeFunction = st.text_input ("Input nilai Diabetes Pedigree Function")
+    DiabetesPedigreeFunction = float(st.text_input("Input nilai diabetes pedigree function") or 0)
+
 
 with col2 :
-    Age = st.text_input ("Input nilai Age")
+    #Age = st.text_input ("Input nilai Age")
+    Age = float(st.text_input("Input nilai age") or 0)
+
 
 # code untuk prediksi
 diab_diagnosis = ""
 
 # membuat tombol untuk prediksi
 if st.button ("Test Prediksi Diabetes"):
-    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, Bloodpressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
     if(diab_prediction[0] == 0):
         diab_diagnosis = "Pasien terkena Diabetes"
